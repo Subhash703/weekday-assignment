df0b604270ffc4b6c4200e2756f76afee95ba9d6
Subhash Chandra <subhash@Subhashs-MacBook-Air.local>
2024-05-13 19:24:23 +0530

    initial setup : project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="App">
+      <img src="https://assets-global.website-files.com/63359abeb97bf0d5ca346052/6336e2cc37e3c15b9c7c4487_Logo_new.png" alt="" className="header-logo" />
+      <h1 className="app_header">
+        Hire from India's largest talent pool
+      </h1>
+    </div>
+  )
+}
+
+export default App
